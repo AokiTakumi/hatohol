8cbe1993a7c53005369adc489e987ee17e56716b
Hiroshi Hatake <hatake@clear-code.com>
2015-06-26 11:34:27 +0900

    [server][HatoholArmPluginGateHAPI2] Add OK clause for self monitoriign triggers

--- server/src/HatoholArmPluginGateHAPI2.cc
+++ server/src/HatoholArmPluginGateHAPI2.cc
@@ -199,6 +199,10 @@ struct HatoholArmPluginGateHAPI2::Impl
 				  HAPI2PluginCollectType::NG_PLUGIN_INTERNAL_ERROR,
 				  HAPI2PluginErrorCode::UNAVAILABLE_HAP2);
 				return;
+			} else {
+				m_impl.setPluginConnectStatus(
+				  HAPI2PluginCollectType::NG_PLUGIN_INTERNAL_ERROR,
+				  HAPI2PluginErrorCode::OK);
 			}
 
 			JSONRPCError errObj;
@@ -212,6 +216,10 @@ struct HatoholArmPluginGateHAPI2::Impl
 				  HAPI2PluginErrorCode::UNAVAILABLE_HAP2);
 
 				return;
+			} else {
+				m_impl.setPluginConnectStatus(
+				  HAPI2PluginCollectType::NG_HATOHOL_INTERNAL_ERROR,
+				  HAPI2PluginErrorCode::OK);
 			}
 
 			m_impl.setPluginConnectStatus(
